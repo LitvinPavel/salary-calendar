3234d1c742596bc6af9fd8702d1baac1875e4d1b
Pavel <litvin.pv@gmail.com>
2022-11-09 17:23:47 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/salary-calendar/",
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/salary-calendar/'
 })
