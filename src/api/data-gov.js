8c63ddfd1f56ccb4895289454ce72a26be2539fe
Pavel <litvin.pv@gmail.com>
2022-11-10 10:07:44 +0300

    fix api url

--- src/api/data-gov.js
+++ src/api/data-gov.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parse, stringify } from 'qs';
 
 const instance = axios.create({
-  baseURL: "http://data.gov.ru/api/json/dataset",
+  baseURL: "https://data.gov.ru/api/json/dataset",
   paramsSerializer: {
     encode: parse,
     serialize: stringify,
