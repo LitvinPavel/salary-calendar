7a39d8f0274d86ee9e87ed04d6e5bdf6330c35c4
Pavel <litvin.pv@gmail.com>
2023-02-07 16:33:10 +0300

    deploy

--- src/api/repository.js
+++ src/api/repository.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { parse, stringify } from 'qs';
 
 const instance = axios.create({
-  baseURL: "https://salary-calendar-api.herokuapp.com/",
+  baseURL: "https://e2e-calendar-api.vercel.app/",
   paramsSerializer: {
     encode: parse,
     serialize: stringify,
